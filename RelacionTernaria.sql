7e2d5b203c6e719da5f633320c43a2478cba03cc
nzhdeh21 <nzhdeh1991@gmail.com>
2017-10-24 18:37:05 +0200

    RelacionTernaria

--- RelacionTernaria.sql
+++ RelacionTernaria.sql
@@ -7,11 +7,13 @@ go
 
 create table Pasajero
 (
-	DNI char(9) not null primary key,
+	DNI char(9) not null,
 	Nombre varchar(20) not null,
 	Apellido varchar(30) not null,
 	Direccion varchar (50) not null,
 	Telefono char (9) not null,
+	--indicamos cual es la clave primaria
+	constraint PK_Pasajero primary key(DNI),
 )
 
 go
@@ -20,50 +22,34 @@ create table Asiento
 (
 	Fila tinyint not null,
 	Numero tinyint not null,
-	constraint PK_Asiento primary key(Fila,Numero),--dos PKs en una misma tabla
+	--indicamos cual es la clave primaria
+	constraint PK_Asiento primary key(Fila,Numero),--dos PKs en una misma tabla(Clave compuesta)
 )
 go
 
 create table Vuelo
 (
-	Codigo tinyint not null primary key,
+	Codigo tinyint not null,
 	Origen varchar(40) not null,
 	Destino varchar(40) not null,
+	--indicamos cual es la clave primaria
+	constraint PK_Vuelo primary key(Codigo),
 )
 
 go
 
-create table PasajeroAsiento
+--Se resuelven igual que las N:M o sea creando una nueva tabla
+
+create table PasajeroAsientoVuelo
 (
-	DNI char(9) not null,
-	Fila tinyint not null,
-	Numero tinyint not null,
-	constraint PK_PasajeroAsiento primary key(DNI,Fila,Numero),
-	constraint Pasajero_Asiento1 foreign key (DNI) references Pasajero (DNI) ON DELETE NO ACTION ON UPDATE CASCADE,
-	constraint Pasajero_Asiento2 foreign key (Fila,Numero) references Asiento (Fila,Numero) ON DELETE NO ACTION ON UPDATE CASCADE,
-)
-
-go
-
-create table PasajeroVuelo
-(
-	DNI char(9) not null,
-	Codigo tinyint not null
-	constraint PK_PasajeroVuelo primary key(DNI,Codigo),
-	constraint Pasajero_Vuelo1 foreign key (DNI) references Pasajero (DNI) ON DELETE NO ACTION ON UPDATE CASCADE,
-	constraint Pasajero_Vuelo2 foreign key (Codigo) references Vuelo (Codigo) ON DELETE NO ACTION ON UPDATE CASCADE,
-)
-
-go
-
-create table VueloAsiento
-(
-	Codigo tinyint not null,
-	Fila tinyint not null,
-	Numero tinyint not null,
-	constraint PK_VueloAsiento primary key(Codigo,Fila,Numero),
-	constraint Vuelo_Asiento1 foreign key (Codigo) references Vuelo (Codigo) ON DELETE NO ACTION ON UPDATE CASCADE,
-	constraint Vuelo_Asiento2 foreign key (Fila,Numero) references Asiento (Fila,Numero) ON DELETE NO ACTION ON UPDATE CASCADE,
+	DNIPasajero char(9) not null,
+	FilaAsiento tinyint not null,
+	NumeroAsiento tinyint not null,
+	CodigoVuelo tinyint not null
+	constraint PK_PasajeroAsiento primary key(DNIPasajero),
+	constraint FK_PasajeroAsiento_Pasajero foreign key (DNIPasajero) references Pasajero (DNI) ON DELETE NO ACTION ON UPDATE CASCADE,
+	constraint FK_PasajeroAsiento_Asiento foreign key (FilaAsiento,CodigoVuelo) references Asiento (Fila,Numero) ON DELETE NO ACTION ON UPDATE CASCADE,
+	constraint FK_PasajeroAsiento_Vuelo foreign key (CodigoVuelo) references Vuelo (Codigo) ON DELETE NO ACTION ON UPDATE CASCADE,
 )
 
 go
