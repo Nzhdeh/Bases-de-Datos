85c513b3353d11fb93bbe6eaee4e27ade4fc8add
nzhdeh21 <nzhdeh1991@gmail.com>
2017-10-24 17:29:26 +0200

    relacion1n

--- Relacion_1N.sql
+++ Relacion_1N.sql
@@ -20,7 +20,7 @@ create table Organizaciones
 	Nombre varchar(20) not null,
 	Campo_Actividad varchar(10) not null,
 	Orientacion_Ideologica varchar(10) not null,
-	DniLideres char(9) not null,
+	DniLideres char(9) not null,--si quieremos que admita valores nulos ponemos NULL en vez de NOT NULL
 	constraint PK_Organizaciones primary key (Nombre),
 	--usamos el metodo de propagacion de la clave primaria de la tabala LIDERES a la tabla ORGANIZACIONES
 	constraint FK_Lideres_Organizaciones foreign key (DniLideres) references Lideres (Dni) on update cascade on delete cascade,
