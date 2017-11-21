c814c284a03ff575dd0a365951f9ce09b27e80c8
nzhdeh21 <nzhdeh1991@gmail.com>
2017-11-21 20:22:19 +0100

    TransLeo

--- TransLeo.sql
+++ TransLeo.sql
@@ -7,18 +7,6 @@ go
 use TransLeo
 go
 
-create table Paquetes
-(
-	Codigo int not null,
-	Alto tinyint not null,
-	Ancho tinyint not null,
-	Largo tinyint not null,
-	Peso tinyint not null,
-
-	---------------------------------------------------------
-	constraint PK_Paquetes primary key (Codigo)
-)
-go
 --es una generalizacion y he decidido hacer una sola tabla
 create table ClientesRemitentesDestinatarios
 (
@@ -35,12 +23,27 @@ create table ClientesRemitentesDestinatarios
 	NombreUsuario varchar (15) not null, 
 	Contraseña varchar (10) not null,
 
-	------Relacion Pemitentes Paquetes
-	CodigoPaquetes int not null,
+	------------------------------------------------------
+	constraint PK_ClientesRemitentesDestinatarios primary key (Codigo),
+)
+go
+
+create table Paquetes
+(
+	Codigo int not null,
+	Alto tinyint not null,
+	Ancho tinyint not null,
+	Largo tinyint not null,
+	Peso tinyint not null,
+	CodigoClientes tinyint not null,
 	NumReferencia int identity not null,
 
+	---------------------------------------------------------
+	constraint PK_Paquetes primary key (Codigo),
+
 	------------------------------------------------------
-	constraint PK_ClientesRemitentesDestinatarios primary key (Codigo)
+	constraint FK_Clientes_Remitentes foreign key (CodigoClientes) references ClientesRemitentesDestinatarios (Codigo) on update cascade on delete cascade,
+	constraint FK_Clientes_Destinatarios foreign key (CodigoClientes) references ClientesRemitentesDestinatarios (Codigo) on update no action on delete no action
 )
 go
 
@@ -54,11 +57,18 @@ create table Centros
 	Provincia varchar (20) not null,
 	Telefono char (9) not null,
 	TelefonoAlternativo char (9) null,
+	Distancia tinyint not null,
+	CodigoPaquetse int not null,
+	FechaRecogida date not null,
+	---------------------------------------------------------
+	constraint PK_Centros primary key (Codigo),
 
 	---------------------------------------------------------
-	constraint PK_Centros primary key (Codigo)
+	constraint FK_Centro_dista_Centro foreign key (Codigo) references Centros (Codigo) on update no action on delete no action,
+	constraint FK_Centro_Paquetse foreign key (CodigoPaquetse) references Paquetes (Codigo) on update no action on delete no action
 )
 go
+
 
 create table Vehiculos
 (
@@ -72,5 +82,24 @@ create table Vehiculos
 
 	---------------------------------------------------------
 	constraint PK_Vehiculos primary key (Matricula)
+)
+go
+
+create table CentroCentroPaqueteVehiculos
+(
+	CodigoCentros tinyint not null,
+	CodigoTrasladaCentros tinyint not null,
+	CodigoPaquetes int not null,
+	MatriculaVehiculos char (7) not null,
+	FechaTraslado date not null,
+	FechaEntrega date not null,
+	---------------------------------------------------------
+	constraint PK_CentroCentroPaqueteVehiculos primary key (CodigoPaquetes),
+
+	----------------------------------------------------------
+	constraint FK_Centro_Centro_Paquete_Vehiculos foreign key (CodigoCentros) references Centros (Codigo) on update cascade on delete cascade,
+	constraint FK_Centro_CentroT_Paquete_Vehiculos foreign key (CodigoTrasladaCentros) references Centros (Codigo) on update no action,
+	constraint FK_Paquete_Centro_CentroT_Vehiculos foreign key (CodigoPaquetes) references Paquetes (Codigo) on update cascade on delete cascade,
+	constraint FK_Vehiculos_Paquete_Centro_CentroT foreign key (MatriculaVehiculos) references Vehiculos (Matricula) on update cascade on delete cascade
 )
 go
