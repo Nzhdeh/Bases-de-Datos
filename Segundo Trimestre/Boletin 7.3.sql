60461fc74fe13e9ce957f9e14bec9ba670ad1754
nzhdeh21 <nzhdeh1991@gmail.com>
2017-12-19 23:01:55 +0100

    Boletin 7.3

---
+++ Segundo Trimestre/Boletin 7.3.sql
@@ -0,0 +1,18 @@
+--Sobre la base de Datos AdventureWorks
+
+use AdventureWorks
+go
+--Nombre, numero de producto, precio y color de los productos de color rojo o amarillo cuyo precio esté comprendido entre 50 y 500
+
+
+--Nombre, número de producto, precio de coste,  precio de venta, margen de beneficios total y margen de beneficios en % del precio 
+--de venta de los productos de categorías inferiores a 16
+
+
+--Empleados cuyo nombre o apellidos contenga la letra "r". Los empleados son los que tienen el valor "EM" en la columna PersonType de la tabla Person
+
+
+--LoginID, nationalIDNumber, edad y puesto de trabajo (jobTitle) de los empleados (tabla Employees) de sexo femenino que tengan más de cinco años de antigüedad
+
+
+--Ciudades correspondientes a los estados 11, 14, 35 o 70, sin repetir. Usar la tabla Person.Address
