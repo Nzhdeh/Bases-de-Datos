9998566e73da75f40569cd52d894a337c1d019fb
nzhdeh21 <nzhdeh1991@gmail.com>
2017-12-22 16:07:23 +0100

    ConsultaBichos

---
+++ Segundo Trimestre/ConsultaBichos.sql
@@ -0,0 +1,29 @@
+--Sobre la base de datos BICHOS.
+use Bichos
+go
+--Introduce dos nuevos clientes. Asígnales los códigos que te parezcan adecuados.
+
+--Introduce una mascota para cada uno de ellos. Asígnales los códigos que te parezcan adecuados.
+
+--Escribe un SELECT para obtener los IDs de las enfermedades que ha sufrido alguna mascota (una cualquiera). Los IDs no deben repetirse
+
+--El cliente Josema Ravilla ha llevado a visita a todas sus mascotas.
+
+--Escribe un SELECT para averiguar el código de Josema Ravilla.
+
+--Escribe otro SELECT para averiguar los códigos de las mascotas de Josema Ravilla.
+
+--Con los códigos obtenidos en la consulta anterior, escribe los INSERT correspondientes en la tabla BI_Visitas. La fecha y hora se tomarán del sistema.
+
+--Todos los perros del cliente 104 han enfermado el 20 de diciembre de sarna.
+
+--Escribe un SELECT para averiguar los códigos de todos los perros del cliente 104
+
+--Con los códigos obtenidos en la consulta anterior, escribe los INSERT correspondientes en la tabla BI_Mascotas_Enfermedades
+
+--Escribe una consulta para obtener el nombre, especie y raza de todas las mascotas, ordenados por edad.
+
+--Escribe los códigos de todas las mascotas que han ido alguna vez al veterinario un lunes o un viernes. Para averiguar el 
+--dia de la semana de una fecha se usa la función DATEPART (WeekDay, fecha) que devuelve un entero entre 1 y 7 donde el 1 
+--corresponde al lunes, el dos al martes y así sucesivamente.
+--NOTA: El servidor se puede configurar para que la semana empiece en lunes o domingo.
