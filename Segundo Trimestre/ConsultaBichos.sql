1fdabd695cbab440ab0c135743670a1d8dfc8b1a
Nzhdeh <nzhdeh1991@gmail.com>
2018-01-15 11:01:40 +0100

    Consulta Bichos

--- Segundo Trimestre/ConsultaBichos.sql
+++ Segundo Trimestre/ConsultaBichos.sql
@@ -24,7 +24,7 @@ go
 --Escribe un SELECT para obtener los IDs de las enfermedades que ha sufrido alguna mascota (una cualquiera). 
 --Los IDs no deben repetirse
 --select * from BI_Mascotas_Enfermedades
-select distinct IDEnfermedad from BI_Mascotas_Enfermedades/************************************/
+select IDEnfermedad from BI_Mascotas_Enfermedades/************************************/
 go
 --El cliente Josema Ravilla ha llevado a visita a todas sus mascotas.
 --Escribe un SELECT para averiguar el código de Josema Ravilla.
@@ -35,6 +35,14 @@ go
 select Codigo from BI_Mascotas
 where CodigoPropietario=102
 go
+
+/*
+select M.Codigo from BI_Mascotas as M
+inner join BI_Clientes as C on M.CodigoPropietario=C.Codigo
+where M.CodigoPropietario in 
+		(select Codigo from BI_Clientes
+			where Nombre='Josema Ravilla')
+			*/
 --Con los códigos obtenidos en la consulta anterior, escribe los INSERT correspondientes en la tabla BI_Visitas. La fecha y hora se tomarán del sistema.
 --select * from BI_Visitas
 insert into BI_Visitas
@@ -49,16 +57,24 @@ go
 --Escribe un SELECT para averiguar los códigos de todos los perros del cliente 104
 --select * from BI_Mascotas_Enfermedades
 --select * from BI_Mascotas
-select * from BI_Mascotas as M
-inner join BI_Mascotas_Enfermedades as ME on M.Codigo=ME.Mascota
-
-
+select Codigo from BI_Mascotas
+where CodigoPropietario=104 and Especie='Perro'
+go
 --Con los códigos obtenidos en la consulta anterior, escribe los INSERT correspondientes en la tabla BI_Mascotas_Enfermedades
-
+--select * from BI_Enfermedades
+insert into BI_Mascotas_Enfermedades
+(IDEnfermedad,Mascota,FechaInicio,FechaCura)
+	values(4,'PH004','20-12-2017',null),
+		  (4,'PH104','20-12-2017',null),
+		  (4,'PM004','20-12-2017',null)
+go
 --Escribe una consulta para obtener el nombre, especie y raza de todas las mascotas, ordenados por edad.
 select Alias,Especie,Raza from BI_Mascotas
 order by FechaNacimiento
+go
 --Escribe los códigos de todas las mascotas que han ido alguna vez al veterinario un lunes o un viernes. Para averiguar el 
 --dia de la semana de una fecha se usa la función DATEPART (WeekDay, fecha) que devuelve un entero entre 1 y 7 donde el 1 
 --corresponde al lunes, el dos al martes y así sucesivamente.
 --NOTA: El servidor se puede configurar para que la semana empiece en lunes o domingo.
+select Mascota from BI_Visitas
+where datepart(weekday,Fecha)=1 or datepart(weekday,Fecha)=5
