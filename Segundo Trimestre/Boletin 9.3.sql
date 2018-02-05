b030f821cac6ad378d67e38e76aed290e76a71a9
Nzhdeh <nzhdeh1991@gmail.com>
2018-02-05 13:54:14 +0100

    Boletin 9.3

---
+++ Segundo Trimestre/Boletin 9.3.sql
@@ -0,0 +1,36 @@
+--Escribe las siguientes consultas sobre la base de datos pubs.
+use pubs
+go
+
+--1. Título y tipo de todos los libros en los que alguno de los autores vive en California (CA).
+
+--2. Título y tipo de todos los libros en los que ninguno de los autores vive en California (CA).
+
+--3. Número de libros en los que ha participado cada autor, incluidos los que no han publicado nada.
+
+--4. Número de libros que ha publicado cada editorial, incluidas las que no han publicado ninguno.
+
+--5. Número de empleados de cada editorial.
+
+--6. Calcular la relación entre número de ejemplares publicados y número de empleados de cada editorial, 
+--incluyendo el nombre de la misma.
+
+--7. Nombre, Apellidos y ciudad de todos los autores que han trabajado para la editorial "Binnet & Hardley” 
+--o "Five Lakes Publishing”
+
+--8. Empleados que hayan trabajado en alguna editorial que haya publicado algún libro en el que alguno de 
+--los autores fuera Marjorie Green o Michael O'Leary.
+
+--9. Número de ejemplares vendidos de cada libro, especificando el título y el tipo.
+
+--10.  Número de ejemplares de todos sus libros que ha vendido cada autor.
+
+--11.  Número de empleados de cada categoría (jobs).
+
+--12.  Número de empleados de cada categoría (jobs) que tiene cada editorial, incluyendo aquellas categorías 
+--en las que no haya ningún empleado.
+
+--13.  Autores que han escrito libros de dos o más tipos diferentes
+
+--14.  Empleados que no trabajan actualmente en editoriales que han publicado libros cuya columna notes 
+--contenga la palabra "and”
