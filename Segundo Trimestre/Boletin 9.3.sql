d877c07d476241836726493068acce9c5b0ed902
Nzhdeh <nzhdeh1991@gmail.com>
2018-02-05 14:26:45 +0100

    Boletin 9.3

--- Segundo Trimestre/Boletin 9.3.sql
+++ Segundo Trimestre/Boletin 9.3.sql
@@ -3,14 +3,35 @@ use pubs
 go
 
 --1. Título y tipo de todos los libros en los que alguno de los autores vive en California (CA).
+select distinct T.title,T.[type] from authors as A 
+inner join titleauthor as TA on A.au_id=A.au_id
+inner join titles as T on TA.title_id=T.title_id
+where A.[state]='CA'
+go
 
 --2. Título y tipo de todos los libros en los que ninguno de los autores vive en California (CA).
+select T.title,T.[type] from titles as T
+except
+select T.title,T.[type] from authors as A 
+inner join titleauthor as TA on A.au_id=A.au_id
+inner join titles as T on TA.title_id=T.title_id
+where A.[state]='CA'
+go
 
 --3. Número de libros en los que ha participado cada autor, incluidos los que no han publicado nada.
+select count(title_id) as [Cantidad de libros],au_id from titleauthor
+group by au_id
+--having count(title_id)>=0
+go
 
 --4. Número de libros que ha publicado cada editorial, incluidas las que no han publicado ninguno.
 
 --5. Número de empleados de cada editorial.
+select P.pub_id,P.pub_name,count(E.emp_id) as [Numero de empleados] from publishers as P
+inner join employee as E on P.pub_id=E.pub_id
+group by P.pub_id, P.pub_name
+order by P.pub_id
+go
 
 --6. Calcular la relación entre número de ejemplares publicados y número de empleados de cada editorial, 
 --incluyendo el nombre de la misma.
