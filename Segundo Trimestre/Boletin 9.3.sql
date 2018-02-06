efd186956fd133bbf7a01af7005bf4a8fe78d036
Nzhdeh <nzhdeh1991@gmail.com>
2018-02-06 12:17:49 +0100

    Boletin 9.3

--- Segundo Trimestre/Boletin 9.3.sql
+++ Segundo Trimestre/Boletin 9.3.sql
@@ -1,15 +1,15 @@
---Escribe las siguientes consultas sobre la base de datos pubs.
+Ôªø--Escribe las siguientes consultas sobre la base de datos pubs.
 use pubs
 go
 
---1. TÌtulo y tipo de todos los libros en los que alguno de los autores vive en California (CA).
+--1. T√≠tulo y tipo de todos los libros en los que alguno de los autores vive en California (CA).
 select distinct T.title,T.[type] from authors as A 
 inner join titleauthor as TA on A.au_id=A.au_id
 inner join titles as T on TA.title_id=T.title_id
 where A.[state]='CA'
 go
 
---2. TÌtulo y tipo de todos los libros en los que ninguno de los autores vive en California (CA).
+--2. T√≠tulo y tipo de todos los libros en los que ninguno de los autores vive en California (CA).
 select T.title,T.[type] from titles as T
 except
 select T.title,T.[type] from authors as A 
@@ -18,40 +18,59 @@ inner join titles as T on TA.title_id=T.title_id
 where A.[state]='CA'
 go
 
---3. N˙mero de libros en los que ha participado cada autor, incluidos los que no han publicado nada.
-select count(title_id) as [Cantidad de libros],au_id from titleauthor
-group by au_id
---having count(title_id)>=0
+--3. N√∫mero de libros en los que ha participado cada autor, incluidos los que no han publicado nada.
+select A.au_id,A.au_fname,A.au_lname,count(TA.title_id) as [Cantidad de libros] from titleauthor as TA
+right join authors as A on TA.au_id=A.au_id
+group by A.au_id,A.au_fname,A.au_lname
 go
 
---4. N˙mero de libros que ha publicado cada editorial, incluidas las que no han publicado ninguno.
-
---5. N˙mero de empleados de cada editorial.
+--4. N√∫mero de libros que ha publicado cada editorial, incluidas las que no han publicado ninguno.
+select P.pub_id,P.pub_name,count (T.title_id) as [Numero de libros] from titles as T
+right join publishers as P on T.pub_id=P.pub_id
+group by P.pub_id,P.pub_name
+go
+--5. N√∫mero de empleados de cada editorial.
 select P.pub_id,P.pub_name,count(E.emp_id) as [Numero de empleados] from publishers as P
 inner join employee as E on P.pub_id=E.pub_id
 group by P.pub_id, P.pub_name
 order by P.pub_id
 go
 
---6. Calcular la relaciÛn entre n˙mero de ejemplares publicados y n˙mero de empleados de cada editorial, 
+--6. Calcular la relaci√≥n entre n√∫mero de ejemplares publicados y n√∫mero de empleados de cada editorial, 
 --incluyendo el nombre de la misma.
+select P.pub_name,(count(T.title_id)/count(E.emp_id)) as [Relacion entre libros publicados y trabajadores] from employee as E
+inner join publishers as P on E.pub_id=P.pub_id
+inner join titles as T on P.pub_id=T.pub_id
+group by P.pub_name
+go
 
---7. Nombre, Apellidos y ciudad de todos los autores que han trabajado para la editorial "Binnet & Hardleyî 
---o "Five Lakes Publishingî
+--7. Nombre, Apellidos y ciudad de todos los autores que han trabajado para la editorial "Binnet & Hardley‚Äù 
+--o "Five Lakes Publishing‚Äù
+select A.au_fname,A.au_lname,A.[state],P.pub_name from authors as A
+inner join titleauthor as TA on A.au_id=TA.au_id
+inner join titles as T on TA.title_id=T.title_id
+inner join publishers as P on T.pub_id=P.pub_id
+where P.pub_name in ('Binnet & Hardley','Five Lakes Publishing')
+go
 
---8. Empleados que hayan trabajado en alguna editorial que haya publicado alg˙n libro en el que alguno de 
+--8. Empleados que hayan trabajado en alguna editorial que haya publicado alg√∫n libro en el que alguno de 
 --los autores fuera Marjorie Green o Michael O'Leary.
 
---9. N˙mero de ejemplares vendidos de cada libro, especificando el tÌtulo y el tipo.
+--9. N√∫mero de ejemplares vendidos de cada libro, especificando el t√≠tulo y el tipo.
 
---10.  N˙mero de ejemplares de todos sus libros que ha vendido cada autor.
+--10.  N√∫mero de ejemplares de todos sus libros que ha vendido cada autor.
 
---11.  N˙mero de empleados de cada categorÌa (jobs).
+--11.  N√∫mero de empleados de cada categor√≠a (jobs).
 
---12.  N˙mero de empleados de cada categorÌa (jobs) que tiene cada editorial, incluyendo aquellas categorÌas 
---en las que no haya ning˙n empleado.
+--12.  N√∫mero de empleados de cada categor√≠a (jobs) que tiene cada editorial, incluyendo aquellas categor√≠as 
+--en las que no haya ning√∫n empleado.
 
---13.  Autores que han escrito libros de dos o m·s tipos diferentes
+--13.  Autores que han escrito libros de dos o m√°s tipos diferentes
 
 --14.  Empleados que no trabajan actualmente en editoriales que han publicado libros cuya columna notes 
---contenga la palabra "andî
+--contenga la palabra "and‚Äù
+
+select * from authors
+--prueba
+insert into authors(au_id,au_lname,au_fname,phone,[address],city,[state],zip,[contract])
+values('172-44-1179','‘µ’≤’´’°’¶’°÷Ä’µ’°’∂','’Ü’™’§’•’∞','628-898-108','Jardin del Eden','Sevilla','AN','12346',0)
