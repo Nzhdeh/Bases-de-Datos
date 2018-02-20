91c59bc230cdc5c6622a29f8bda51b351460dcfe
Nzhdeh <nzhdeh1991@gmail.com>
2018-02-20 12:29:26 +0100

    Boletin 10.1

--- Segundo Trimestre/Boletin 10.1.sql
+++ Segundo Trimestre/Boletin 10.1.sql
@@ -2,8 +2,33 @@ use AdventureWorks2012
 go
 
 --1. Nombre y dirección completas de todos los clientes que tengan alguna sede en Canada.
+select * from Person.Person as P
+inner join Sales.Customer as C on P.BusinessEntityID=C.PersonID
+inner join Sales.SalesOrderHeader as SOH on c.CustomerID=SOH.CustomerID
+
 
 --2. Nombre de cada categoría y producto más caro y más barato de la misma, incluyendo los precios.
+
+--select P.[Name],PK.[Name],max(P.ListPrice) as [Precio maximo],min(P.ListPrice) as [Precio minimo] from Production.ProductCategory as PK
+--inner join Production.ProductSubcategory as PSC on PK.ProductCategoryID=PSC.ProductCategoryID
+--inner join Production.Product as P on PSC.ProductSubcategoryID=P.ProductSubcategoryID
+--group by  P.[Name],PK.[Name] 
+go
+create view ProductosPorPrecioYNombre as
+select P.[Name] as NombreProducto,PK.[Name],P.ListPrice from Production.ProductCategory as PK
+inner join Production.ProductSubcategory as PSC on PK.ProductCategoryID=PSC.ProductCategoryID
+inner join Production.Product as P on PSC.ProductSubcategoryID=P.ProductSubcategoryID
+group by  P.[Name],PK.[Name],P.ListPrice 
+go
+
+select PN.NombreProducto,PN.[Name],PrecioMaxMin.PrecioMaximo as Maximo,PrecioMaxMin.PrecioMinimo as Minimo from ProductosPorPrecioYNombre as PN
+inner join 
+(
+	select PN.[Name] as NombreProducto,PN.[Name],max(PN.ListPrice) as PrecioMaximo,min(PN.ListPrice) as PrecioMinimo from ProductosPorPrecioYNombre as PN
+	group by NombreProducto,PN.[Name]
+) as PrecioMaxMin on PN.NombreProducto=PrecioMaxMin.NombreProducto
+group by PN.NombreProducto,PN.[Name],PrecioMaxMin.PrecioMaximo,PrecioMaxMin.PrecioMinimo
+
 
 --3. Total de Ventas en cada país en dinero (Ya hecha en el boletín 9.3).
 
