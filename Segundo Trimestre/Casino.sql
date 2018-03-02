a68efc2a4e7e38671dc7a3e1037936f97b16b174
Nzhdeh <nzhdeh1991@gmail.com>
2018-03-02 11:00:01 +0100

    Casino tema 10

---
+++ Segundo Trimestre/Casino.sql
@@ -0,0 +1,48 @@
+use CasinOnLine2
+go
+
+--Especificaciones
+--Cada apuesta tiene una serie de números (entre 1 y 24 números) asociados en la tabla COL_NumerosApuestas. 
+--La apuesta es ganadora si alguno de esos números coincide con el número ganador de la jugada y perdedora en caso contrario.
+
+--Si la apuesta es ganadora, el jugador recibe lo apostado (Importe) multiplicado por el valor de la columna Premio de la 
+--tabla COL_TiposApuestas que corresponda con el tipo de la apuesta. Si la apuesta es perdedora, el jugador pierde lo que haya apostado (Importe)
+
+--Ejercicio 1
+--Escribe una consulta que nos devuelva el número de veces que se ha apostado a cada número con apuestas de los tipos 10, 13 o 15.
+--Ordena el resultado de mayor a menos popularidad.
+select J.Numero,A.Tipo,count(*) as [Numero de veces apostados] from COL_Jugadas as J
+inner join COL_Apuestas as A on J.IDJugada=A.IDJugada
+where A.Tipo in(10,13,15)
+group by J.Numero,A.Tipo
+order by [Numero de veces apostados] desc
+
+--Ejercicio 2
+--El casino quiere fomentar la participación y decide regalar saldo extra a los jugadores que hayan apostado más de tres 
+--veces en el último mes. Se considera el mes de febrero.
+--La cantidad que se les regalará será un 5% del total que hayan apostado en ese mes
+
+select A.IDJugador,count(A.IDJugada) as [Veces apostados],sum(A.Importe) as [Apuesta Total por jugador],TA.Premio,(TA.Premio+(TA.Premio*0.05)) as [Premio mas regalo] from COL_Apuestas as A
+inner join COL_Jugadas as J on A.IDJugada=J.IDJugada
+inner join COL_TiposApuesta as TA on A.Tipo=TA.ID
+where month(J.MomentoJuega)=2
+group by A.IDJugador,TA.Premio
+having count(A.IDJugada)>3
+
+--Ejercicio 3
+--El día 2 de febrero se celebró el día de la marmota. Para conmemorarlo, el casino ha decidido volver a repetir 
+--todas las jugadas que se hicieron ese día, pero poniéndoles fecha de mañana (con la misma hora) y permitiendo que 
+--los jugadores apuesten. El número ganador de cada jugada se pondrá a NULL y el NoVaMas a 0.
+--Crea esas nuevas filas con una instrucción INSERT-SELECT
+
+--Ejercicio 4
+--Crea una vista que nos muestre, para cada jugador, nombre, apellidos, Nick, número de apuestas realizadas, 
+--total de dinero apostado y total de dinero ganado/perdido.
+
+--Ejercicio 5
+--Nos comunican que la policía ha detenido a nuestro cliente Ombligo Pato por delitos de estafa, falsedad, 
+--administración desleal y mal gusto para comprar bañadores. 
+--Dado que nuestro casino está en Gibraltar, siguiendo la tradición de estas tierras, queremos borrar todo 
+--rastro de su paso por nuestro casino.
+--Borra todas las apuestas que haya realizado, pero no busques su ID a mano en la tabla COL_Clientes. 
+--Utiliza su Nick (bankiaman) para identificarlo en la instrucción DELETE.
