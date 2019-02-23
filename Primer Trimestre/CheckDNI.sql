18b6e067c3a03164bb07d03c05fc793270762bc8
nzhdeh21 <nzhdeh1991@gmail.com>
2019-02-23 16:10:29 +0100

    dshgkjgh
    fdgdfh

--- Primer Trimestre/CheckDNI.sql
+++ Primer Trimestre/CheckDNI.sql
@@ -13,13 +13,16 @@ create table Persona
 	Telefono char (9) null,
 	Email varchar (30) null,
 
-	-----------------------
-	constraint PK_Persona primary key (DNI)
+	-----------------PK-----------------------------
+	constraint PK_Persona primary key (DNI),
+
+	-----------------CK------------------------------------------
+	constraint CK_CK_DNI check (DNI like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][A-Z]')
 )
 go
 
-alter table Persona add constraint CK_DNI check (DNI like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][A-Z]')
-go
+--alter table Persona add constraint CK_DNI check (DNI like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][A-Z]')
+--go
 
 alter table Persona add constraint CK_Telefono check (Telefono like '[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
 go
@@ -27,8 +30,19 @@ go
 alter table Persona add constraint CK_Email check (Email like '%@%.%')
 go
 
-insert into Persona values 
-('22446678E','899999299','nzhdeh1991@gmail.com')
+insert into Persona 
+	   (DNI,Telefono,Email)
+values ('22446678E','899999299','nzhdeh1991@gmail.com')
 go
+
+--insert into Persona 
+-- 	     (DNI,Telefono,Email)
+--values ('22446678J','89999929','nzhdeh1991gmail.com')	--caso de error
+--go
+
+--insert into Persona 
+-- 	     (DNI,Telefono,Email)
+--values ('22446678J','899999293','nzhdeh1991gmail.com')	--caso de error
+--go
 
 select * from Persona
